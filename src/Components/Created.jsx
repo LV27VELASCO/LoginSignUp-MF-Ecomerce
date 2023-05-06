5aa405c7780d0cfd7b45e97019a229396264dced
Luis Velasco <luiselsw27@gmail.com>
2023-05-06 01:52:04 -0500

    agregando logout y addcart

--- src/Components/Created.jsx
+++ src/Components/Created.jsx
@@ -5,7 +5,7 @@ const Created = () => {
   return (
     <div className="m-auto flex flex-col items-center max-w-max">
         <div className="bg-green-400 rounded-full w-24 h-24 p-2 flex items-center justify-center">
-        <i class="fa-solid fa-face-smile-wink text-5xl text-white"></i>
+        <i className="fa-solid fa-face-smile-wink text-5xl text-white"></i>
         </div>
         <div className="bg-green-400 px-5 py-2 rounded-md">
         <h3 className="font-semibold text-gray-50">User created successfully!!</h3>
